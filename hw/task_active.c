bf3b8893c5c2f0c8d16b0da31be93eed70fddae9
Softwarebugger <hlyu42@wisc.edu>
2023-12-10 16:14:05 -0600

    bounce

--- hw/task_active.c
+++ hw/task_active.c
@@ -8,12 +8,19 @@ int offset = SCREEN_X/8;
 void task_update(void *pvParameters) {
     while (1) {
         static uint8_t counter = 0;
-        counter = (counter + 1)%5;
+        counter = (counter + 1)%1;
         if (player1_claimed && cleared) {
             if (!serve_ball) {
-                balldy = (rand() % 2);
+                balldy = (rand() % 3);
                 serve_ball = true;
             }
+            else if (ballX - playerX <= (5 + (ballWidthPixels + paddleLeftWidthPixels)/2) 
+                  && ballX - playerX >= -(5 + (ballWidthPixels + paddleLeftWidthPixels)/2)  
+                  && ballY - playerY <= (5 + (ballHeightPixels + paddleLeftHeightPixels)/2)
+                  && ballY - playerY >= -(5 + (ballHeightPixels + paddleLeftHeightPixels)/2)) {
+                    balldy = (rand() % 3);
+                    balldx = -balldx;
+                  }
             else if (ballX <= ballWidthPixels/2 + 2 || ballX >= SCREEN_X - ballWidthPixels/2 - 2) balldx = -balldx;
             else if (ballY <= ballHeightPixels/2 + 2 || ballY >= SCREEN_Y - ballHeightPixels/2 - 2) balldy = -balldy;
             if (counter == 0) {
@@ -21,6 +28,9 @@ void task_update(void *pvParameters) {
                 ballY = ballY + balldy; 
             }         
             lcd_draw_image(ballX, ballY, ballHeightPixels, ballWidthPixels, ballBitmaps, LCD_COLOR_ORANGE, LCD_COLOR_BLACK);
+            uint16_t fcolor = LCD_COLOR_BLUE;
+            if (isplayer1) fcolor = LCD_COLOR_RED;
+            lcd_draw_image(playerX, playerY, paddleLeftHeightPixels, paddleLeftWidthPixels, paddleLeftBitmaps, fcolor, LCD_COLOR_BLACK);
         }
     }
 }
@@ -33,22 +43,12 @@ void task_draw_player(void *pvParameters) {
                 cleared = true;
             }
             else {
-                uint16_t fcolor = LCD_COLOR_BLUE;
-                if (isplayer1) fcolor = LCD_COLOR_RED;
-                //lcd_draw_image(playerX, playerY, paddleLeftHeightPixels, paddleLeftWidthPixels, paddleLeftBitmaps, fcolor, LCD_COLOR_BLACK);
-                
             }
         }
-    }
-}
-void task_draw_ball(void *pvParameters) {
-    while (1) {
-        lcd_draw_image(ballX, ballY, ballHeightPixels, ballWidthPixels, ballBitmaps, LCD_COLOR_ORANGE, LCD_COLOR_BLACK);
     }
 }
 
 void task_active_init() {
     xTaskCreate(task_draw_player, "Active task", configMINIMAL_STACK_SIZE, NULL, 2, &draw_player_task);
     xTaskCreate(task_update, "update positions", configMINIMAL_STACK_SIZE, NULL, 2, &update_task);
-    //xTaskCreate(task_draw_ball, "draw ball task", configMINIMAL_STACK_SIZE, NULL, 2, &draw_ball_task);
 }
